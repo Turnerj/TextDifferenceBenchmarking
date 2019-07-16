1a58403883384d23800600e9e91a087d91e79e46
Turnerj <git@turnerj.com>
2019-07-16 17:11:23 +0930

    Added DenseMatrix version (ImageSharp DenseMatrix)
    https://github.com/SixLabors/ImageSharp/blob/a48de6301b4e5a410e7b6fbd3512fe3c38ee2fc1/src/ImageSharp/Primitives/DenseMatrix%7BT%7D.cs

---
+++ TextDifferenceBenchmarking/Utilities/InliningOptions.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+using System.Text;
+
+namespace TextDifferenceBenchmarking.Utilities
+{
+	/// <summary>
+	/// Global inlining options. Helps temporarily disable inlining for better profiler output.
+	/// </summary>
+	internal static class InliningOptions
+	{
+#if PROFILING
+        public const MethodImplOptions ShortMethod = MethodImplOptions.NoInlining;
+#else
+		public const MethodImplOptions ShortMethod = MethodImplOptions.AggressiveInlining;
+#endif
+		public const MethodImplOptions ColdPath = MethodImplOptions.NoInlining;
+	}
+}
