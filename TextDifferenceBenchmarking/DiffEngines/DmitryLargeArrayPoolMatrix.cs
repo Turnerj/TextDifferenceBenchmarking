86605dd8cb4b43e384f7116881ce7c57d7aa6e43
Turnerj <git@turnerj.com>
2019-07-18 14:51:33 +0930

    Memory-efficient Large Array Pool Matrix benchmark

---
+++ TextDifferenceBenchmarking/DiffEngines/DmitryLargeArrayPoolMatrix.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Buffers;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using TextDifferenceBenchmarking.Utilities;
+
+namespace TextDifferenceBenchmarking.DiffEngines
+{
+	/// <summary>
+	/// Dmitry Bychenko's solution but with an ArrayPool-based inline matrix (based on DenseMatrix)
+	/// </summary>
+	public class DmitryLargeArrayPoolMatrix : ITextDiff
+	{
+		public EditOperation[] EditSequence(
+			string source, string target,
+			int insertCost = 1, int removeCost = 1, int editCost = 1)
+		{
+
+			if (null == source)
+				throw new ArgumentNullException("source");
+			else if (null == target)
+				throw new ArgumentNullException("target");
+
+			// Forward: building score matrix
+
+			var columns = target.Length + 1;
+			var rows = source.Length + 1;
+			var mPool = new LargeArrayPoolMatrix<EditOperationKind>(columns, rows);
+			var dPool = new LargeArrayPoolMatrix<int>(columns, rows);
+
+			// Best operation (among insert, update, delete) to perform 
+			var M = mPool;
+
+			// Minimum cost so far
+			var D = dPool;
+
+			// Edge: all removes
+			for (int i = 1; i <= source.Length; ++i)
+			{
+				M[i, 0] = EditOperationKind.Remove;
+				D[i, 0] = removeCost * i;
+			}
+
+			// Edge: all inserts 
+			for (int i = 1; i <= target.Length; ++i)
+			{
+				M[0, i] = EditOperationKind.Add;
+				D[0, i] = insertCost * i;
+			}
+
+			// Having fit N - 1, K - 1 characters let's fit N, K
+			for (int i = 1; i <= source.Length; ++i)
+				for (int j = 1; j <= target.Length; ++j)
+				{
+					// here we choose the operation with the least cost
+					int insert = D[i, j - 1] + insertCost;
+					int delete = D[i - 1, j] + removeCost;
+					int edit = D[i - 1, j - 1] + (source[i - 1] == target[j - 1] ? 0 : editCost);
+
+					int min = Math.Min(Math.Min(insert, delete), edit);
+
+					if (min == insert)
+						M[i, j] = EditOperationKind.Add;
+					else if (min == delete)
+						M[i, j] = EditOperationKind.Remove;
+					else if (min == edit)
+						M[i, j] = EditOperationKind.Edit;
+
+					D[i, j] = min;
+				}
+
+			// Backward: knowing scores (D) and actions (M) let's building edit sequence
+			List<EditOperation> result =
+			  new List<EditOperation>(source.Length + target.Length);
+
+			for (int x = target.Length, y = source.Length; (x > 0) || (y > 0);)
+			{
+				EditOperationKind op = M[y, x];
+
+				if (op == EditOperationKind.Add)
+				{
+					x -= 1;
+					result.Add(new EditOperation('\0', target[x], EditOperationKind.Add));
+				}
+				else if (op == EditOperationKind.Remove)
+				{
+					y -= 1;
+					result.Add(new EditOperation(source[y], '\0', EditOperationKind.Remove));
+				}
+				else if (op == EditOperationKind.Edit)
+				{
+					x -= 1;
+					y -= 1;
+					result.Add(new EditOperation(source[y], target[x], EditOperationKind.Edit));
+				}
+				else // Start of the matching (EditOperationKind.None)
+					break;
+			}
+
+			dPool.Dispose();
+			mPool.Dispose();
+
+			result.Reverse();
+
+			return result.ToArray();
+		}
+	}
+}
---
+++ TextDifferenceBenchmarking/Utilities/LargeArrayPoolMatrix.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Buffers;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Drawing;
+using System.Runtime.CompilerServices;
+using System.Text;
+
+namespace TextDifferenceBenchmarking.Utilities
+{
+	/// <summary>
+	/// Every row in the array is an ArrayPool of {columns}, extending the upper bound of the shared ArrayPool
+	/// </summary>
+	/// <typeparam name="T">The type of elements in the matrix.</typeparam>
+	public readonly struct LargeArrayPoolMatrix<T> : IDisposable
+		where T : struct
+	{
+		private const int MaxSharedArrayPoolSize = 1048576;
+		
+		private readonly ArrayPool<T> Pool;
+		private readonly int PoolCount;
+		private readonly int PoolSize;
+		
+		private readonly T[][] Data;
+
+		/// <summary>
+		/// Gets the number of columns in the dense matrix.
+		/// </summary>
+		public readonly int Columns;
+
+		/// <summary>
+		/// Gets the number of rows in the dense matrix.
+		/// </summary>
+		public readonly int Rows;
+		
+		/// <summary>
+		/// Initializes a new instance of the <see cref=" DenseMatrix{T}" /> struct.
+		/// </summary>
+		/// <param name="columns">The number of columns.</param>
+		/// <param name="rows">The number of rows.</param>
+		public LargeArrayPoolMatrix(int columns, int rows)
+		{
+			Pool = ArrayPool<T>.Shared;
+			Rows = rows;
+			Columns = columns;
+			PoolSize = MaxSharedArrayPoolSize;
+			
+			var leftOver = columns * rows;
+			var numberOfFullPools = 0;
+			for (; leftOver >= PoolSize; leftOver -= PoolSize, numberOfFullPools++);
+
+			PoolCount = numberOfFullPools;
+			if (leftOver > 0)
+			{
+				PoolCount++;
+			}
+
+			Data = new T[PoolCount][];
+
+			for (int i = 0; i < PoolCount; i++)
+			{
+				var poolRent = PoolSize;
+				if (i + 1 == PoolCount && leftOver > 0)
+				{
+					poolRent = leftOver;
+				}
+				Data[i] = Pool.Rent(poolRent);
+			}
+		}
+
+		/// <summary>
+		/// Gets or sets the item at the specified position.
+		/// </summary>
+		/// <param name="row">The row-coordinate of the item. Must be greater than or equal to zero and less than the height of the array.</param>
+		/// <param name="column">The column-coordinate of the item. Must be greater than or equal to zero and less than the width of the array.</param>
+		/// <returns>The <see typeparam="T"/> at the specified position.</returns>
+		public ref T this[int row, int column]
+		{
+			[MethodImpl(InliningOptions.ShortMethod)]
+			get
+			{
+				if (row >= Rows)
+				{
+					throw new ArgumentException("Invalid row index");
+				}
+				if (column >= Columns)
+				{
+					throw new ArgumentException("Invalid column index");
+				}
+
+				var itemIndex = (row * this.Columns) + column;
+				var poolIndex = 0;
+				for (; itemIndex >= PoolSize; itemIndex -= PoolSize, poolIndex++);
+				return ref Data[poolIndex][itemIndex];
+			}
+		}
+
+		public void Dispose()
+		{
+			for (int i = 0; i < PoolCount; i++)
+			{
+				Pool.Return(Data[i], true);
+			}
+		}
+
+		/// <inheritdoc/>
+		public override bool Equals(object obj) => obj is LargeArrayPoolMatrix<T> other && this.Equals(other);
+
+		/// <inheritdoc/>
+		public bool Equals(LargeArrayPoolMatrix<T> other) =>
+			this.Columns == other.Columns
+			&& this.Rows == other.Rows
+			&& this.Data.Equals(other.Data);
+
+		/// <inheritdoc/>
+		public override int GetHashCode() => this.Data.GetHashCode();
+	}
+}
